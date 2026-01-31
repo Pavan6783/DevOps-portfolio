9ddd31186381b43e39eb26eda72a70bcc38018bf
Dondapati Pavan Kumar <130956905+Pavan6783@users.noreply.github.com>
2026-01-31 15:26:32 +0530

    Create Hero.tsx

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+  return (
+    <section className="h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold text-neon">
+        Pavan Kumar Dondapati
+      </h1>
+      <p className="mt-4 text-xl">
+        DevOps and Cloud Engineer
+      </p>
+    </section>
+  )
+}
