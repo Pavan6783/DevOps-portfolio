ebc6c084bdb433283a871edb2d20fd7289f50058
Dondapati Pavan Kumar <130956905+Pavan6783@users.noreply.github.com>
2026-01-31 15:28:52 +0530

    Create Contact.tsx

---
+++ src/components/Contact.tsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <section id="contact" className="p-20 text-center">
+      <h2 className="text-3xl text-neon mb-6">Contact</h2>
+      <p>📞 123456789</p>
+      <p>🔗 LinkedIn.com</p>
+    </section>
+  )
+}
