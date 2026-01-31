5c6092e9735db1279d4bb88bf0b6e0ee6343acc4
Dondapati Pavan Kumar <130956905+Pavan6783@users.noreply.github.com>
2026-01-31 15:30:48 +0530

    Create projects.ts

---
+++ src/data/projects.ts
@@ -0,0 +1,7 @@
+export const projects = [
+  {
+    title: "AWS 3-Tier Architecture",
+    description:
+      "Designed and deployed a highly available AWS 3-tier architecture using VPC, EC2, ALB, Auto Scaling, and RDS."
+  }
+]
