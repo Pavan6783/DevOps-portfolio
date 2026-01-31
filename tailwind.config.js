b7ed90424b9e3fd3f930c645e7e306abf72dc5b4
Dondapati Pavan Kumar <130956905+Pavan6783@users.noreply.github.com>
2026-01-31 15:19:48 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        cyber: "#020617",
+        neon: "#00f5ff"
+      }
+    }
+  },
+  plugins: []
+}
